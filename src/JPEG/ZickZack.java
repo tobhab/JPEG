7d14699134c78909db74062e064b6bae62ecaa1e
SebastianHerr <Sebastian-Herr@web.de>
2018-11-17 20:40:11 +0100

    Simplified the class for zickzack-encoding and added a class for inverse zickzack-encoding

--- src/JPEG/ZickZack.java
+++ src/JPEG/ZickZack.java
@@ -3,16 +3,30 @@ package JPEG;
 public class ZickZack {
 
   private int N;
-  private int index = 0;
+  private int offsetForBlock = 0;
   private int[] result;
+
+  //The value in this field indicates at which index-1 in the 1d final array a cell ends up
+  private final int[][] zickZackMapping8 = {
+          {1, 2, 6, 7, 15, 16, 28, 29},
+          {3, 5, 8, 14, 17, 27, 30, 43},
+          {4, 9, 13, 18, 26, 31, 42, 44},
+          {10, 12, 19, 25, 32, 41, 45, 54},
+          {11, 20, 24, 33, 40, 46, 53, 55},
+          {21, 23, 34, 39, 47, 52, 56, 61},
+          {22, 35, 38, 48, 51, 57, 60, 62},
+          {36, 37, 49, 50, 58, 59, 63, 64}};
 
   /**
    * DC coding for each depending block (in a row) and build Zig-Zag sequence (block to array).
-   * 
+   *
    * @param arr
    * @param block_size
    */
-  public ZickZack(int[][] arr, int block_size) {
+  public ZickZack(int[][] arr, int block_size) throws Exception {
+    if (block_size != 8) {
+      throw new Exception("Zickzack is only supported on a blocksize of 8!");
+    }
 
     N = block_size;
     int width = arr[0].length;
@@ -35,51 +49,17 @@ public class ZickZack {
     int rows = y_block;
     int columns = x_block;
 
-    boolean diagonal = true; // true = up, false = down
-
     DC_temp = arr[rows][columns];
     arr[rows][columns] -= DC;
-    result[index++] = arr[rows][columns];
-    while (true) {
-      if (rows == y_block + N - 1 && columns == x_block + N - 1)
-        break;
 
-      if (rows == y_block) { // border up
-        if (columns + 1 == x_block + N)
-          result[index++] = arr[++rows][columns];
-        else
-          result[index++] = arr[rows][++columns];
-        result[index++] = arr[++rows][--columns];
-        diagonal = true;
-      } else if (columns == x_block) { // border left
-        if (rows + 1 == y_block + N)
-          result[index++] = arr[rows][++columns];
-        else
-          result[index++] = arr[++rows][columns];
-        result[index++] = arr[--rows][++columns];
-        diagonal = false;
-      } else if (rows + 1 == y_block + N) { // border bottom
-        result[index++] = arr[rows][++columns];
-        if (!(rows == y_block + N - 1 && columns == x_block + N - 1))
-          result[index++] = arr[--rows][++columns];
-        diagonal = false;
-      } else if (columns + 1 == x_block + N) { // border right
-        result[index++] = arr[++rows][columns];
-        if (rows != y_block + N && columns != x_block + N)
-          result[index++] = arr[++rows][--columns];
-        diagonal = true;
-      } else { // inside of block
-        if (diagonal)
-          result[index++] = arr[++rows][--columns];
-        else
-          result[index++] = arr[--rows][++columns];
+    for (int x = 0; x < N; x++) {
+      for (int y = 0; y < N; y++) {
+        int indexInBlock = zickZackMapping8[x][y] - 1;
+        result[offsetForBlock + indexInBlock] = arr[x_block + x][y_block + y];
       }
     }
+    offsetForBlock += N * N;
     return DC_temp;
-  }
-
-  public void doIt(int[][] arr, int row, int column) {
-    result[index++] = arr[row][column];
   }
 
   public int[] getResult() {
---
+++ src/JPEG/ZickZackInverse.java
@@ -0,0 +1,72 @@
+package JPEG;
+
+public class ZickZackInverse {
+
+  private int N;
+  private int index = 0;
+  private int[][] result;
+
+  //The inner array represents at which location in the 2d array the location in the 1d array ends up
+  private int[][] ReverseZickZackMapping8 = {
+          {0, 0}, {0, 1}, {1, 0}, {2, 0}, {1, 1}, {0, 2}, {0, 3}, {1, 2},
+          {2, 1}, {3, 0}, {4, 0}, {3, 1}, {2, 2}, {1, 3}, {0, 4}, {0, 5},
+          {1, 4}, {2, 3}, {3, 2}, {4, 1}, {5, 0}, {6, 0}, {5, 1}, {4, 2},
+          {3, 3}, {2, 4}, {1, 5}, {0, 6}, {0, 7}, {1, 6}, {2, 5}, {3, 4},
+          {4, 3}, {5, 2}, {6, 1}, {7, 0}, {7, 1}, {6, 2}, {5, 3}, {4, 4},
+          {3, 5}, {2, 6}, {1, 7}, {2, 7}, {3, 6}, {4, 5}, {5, 4}, {6, 3},
+          {7, 2}, {7, 3}, {6, 4}, {5, 5}, {4, 6}, {3, 7}, {4, 7}, {5, 6},
+          {6, 5}, {7, 4}, {7, 5}, {6, 6}, {5, 7}, {6, 7}, {7, 6}, {7, 7}};
+
+  /**
+   * DC coding for each depending block (in a row) and build Zig-Zag sequence (block to array).
+   *
+   * @param arr
+   * @param block_size
+   */
+  public ZickZackInverse(int[] arr, int block_size, int width, int height) throws Exception {
+    if (block_size != 8) {
+      throw new Exception("Inverse Zickzack is only supported on a blocksize of 8!");
+    }
+
+
+    N = block_size;
+
+    result = new int[width][height];
+
+    int offset = 0;
+    int DC = 0;
+    // for each block of image
+    for (int y_block = 0; y_block < height; y_block += N) {
+      DC = 0;
+      for (int x_block = 0; x_block < width; x_block += N) {
+        DC = zickzackInverse(arr, offset, y_block, x_block, DC);
+        offset += N * N;
+      }
+    }
+  }
+
+  public int zickzackInverse(int[] arr, int offsetInArr, int y_block, int x_block, int DC) throws Error {
+    int DC_temp;
+
+    int rows = y_block;
+    int columns = x_block;
+
+    boolean diagonal = true; // true = up, false = down
+
+    DC_temp = arr[columns];
+    arr[columns] -= DC;
+
+    for (int i = 0; i < (N * N); i++) {
+      int x_offset = ReverseZickZackMapping8[i][0];
+      int y_offset = ReverseZickZackMapping8[i][1];
+      result[x_block + x_offset][y_block + y_offset] = arr[offsetInArr + i];
+    }
+
+    return DC_temp;
+  }
+
+  public int[][] getResult() {
+    return result;
+  }
+
+}
